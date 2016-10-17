8798bb4266bb596882b8fd6007d06bcccb4c2d15
ivanbuhov <ivanbuhov@gmail.com>
2016-10-17 17:46:29 +0300

    Add ios implementation

---
+++ app/grayscale-algo.ios.js
@@ -0,0 +1,33 @@
+function toGrayScale(bitmapSrc, bitmapDest, appDir, progressCallback) {
+    var destinationOutputPath = NSString.stringWithString(appDir).stringByAppendingPathComponent(bitmapDest);
+    console.log('--> source: ' + bitmapSrc);
+    console.log('--> destination: ' + destinationOutputPath);
+
+    var image = UIImage.imageWithContentsOfFile(bitmapSrc);
+    var actualWidth = image.size.width;
+    var actualHeight = image.size.height;
+    var imageRect = CGRectMake(0, 0, actualWidth, actualHeight);
+    var colorSpace = CGColorSpaceCreateDeviceGray();
+
+    var context = CGBitmapContextCreate(null, actualWidth, actualHeight, 8, 0, colorSpace, kCGImageAlphaNone);
+    CGContextDrawImage(context, imageRect, image.CGImage);
+    var grayImage = CGBitmapContextCreateImage(context);
+
+    var alphaContext = CGBitmapContextCreate(null, actualWidth, actualHeight, 8, 0, null, kCGImageAlphaOnly);
+    CGContextDrawImage(alphaContext, imageRect, image.CGImage);
+    var mask = CGBitmapContextCreateImage(alphaContext);
+
+    var grayScaleImage = UIImage.imageWithCGImageScaleOrientation(CGImageCreateWithMask(grayImage, mask), image.scale, image.imageOrientation);
+    var grayScaleImageBinary = UIImagePNGRepresentation(grayScaleImage);
+    grayScaleImageBinary.writeToFileAtomically(destinationOutputPath, true);
+
+    //CGColorSpaceRelease(colorSpace);
+    //CGContextRelease(context);
+    //CGContextRelease(alphaContext);
+    //CGImageRelease(grayImage);
+    //CGImageRelease(mask);
+
+    return destinationOutputPath;
+}
+
+module.exports.ToGrayscale = toGrayScale;
