760ccafec3c607fb57ac9d2eb5b978d997aedc5d
sis0k0 <stanimira.vlaeva@gmail.com>
2018-05-20 20:11:56 +0300

    chore: update to webpack 4 and {N} 4

--- app/app.js
+++ app/app.js
@@ -1,4 +1,2 @@
-require("./bundle-config");
-
 var application = require("application");
 application.start({ moduleName: "main-page" });
--- app/bundle-config.js
+++
@@ -1,3 +0,0 @@
-if (global.TNS_WEBPACK) {
-    global.registerModule("main-page", () => require("./main-page"));
-}
